1b528e2ffaf7beffd84ae11843afe3612e4f703a
GustavoGussolli <134729255+GustavoGussolli@users.noreply.github.com>
2023-10-06 14:55:45 -0300

    Create safadometro_tela.php

---
+++ safadometro_tela.php
@@ -0,0 +1,16 @@
+<h1>Safadometro</h1>
+
+<form>
+
+    <input type="text" placeholder="Informe o dia">
+    <br/>
+    <input type="text" placeholder="Informe o mês">
+    <br/>
+    <input type="text" placeholder="Informe o ano">
+    <br/>
+
+    <input type="submit" value="calcular">
+
+</form>
+
+<img src="https://i1.sndcdn.com/artworks-000081526337-nluab6-t500x500.jpg">
